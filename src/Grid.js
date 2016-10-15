5a739c17adcf6f3e6e22f58d79dd9a12abd8cba8
marckimbrey <marckimbrey@gmail.com>
2016-10-15 07:56:29 +0100

    Replace canvas with table

--- src/Grid.js
+++ src/Grid.js
@@ -1,30 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
+import GridRow from './GridRow';
 
-export default class Grid extends Component {
-  constructor(props) {
-    super();
+export default (props) => {
+    let grid = props.grid.map((row, x)=> {
+      return <GridRow gridRow={props.grid[x]} key={x}/>;
+    });
 
-    this.grid = props.grid;
-  }
-  componentDidMount() {
-    this.updateCanvas();
-  }
-  componentWillUpdate() {
-    this.updateCanvas();
-  }
-  updateCanvas() {
-    const ctx = this.refs.canvas.getContext('2d');
-    ctx.clearRect(0,0,400,400);
-      this.props.grid.forEach((row, x)=> {
-        row.forEach((cell, y)=> {
-          if(cell === 1) {
-            ctx.fillStyle = "rgb(200,0,0)";
-            ctx.fillRect(x*8, y*8, 8, 8);
-          }
-        });
-      });
-  }
-  render() {
-    return(<canvas ref="canvas" width={400} height={400}></canvas>);
-  }
+    return(<table><tbody>{grid}</tbody></table>);
 }
